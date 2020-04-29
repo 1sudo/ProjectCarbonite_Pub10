5a62d5bfda737cfe95bb79ca4ebb8e772a04d292
G-Star <xxeus11@gmail.com>
2020-04-29 12:34:40 -0500

    missing trophy files

---
+++ MMOCoreORB/bin/scripts/loot/items/custom/bh_skull.lua
@@ -0,0 +1,12 @@
+bh_skull = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/loot/misc/loot_skull_human.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("bh_skull", bh_skull)
