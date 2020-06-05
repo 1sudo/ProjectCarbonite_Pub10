38128a282ee4f1198150b0e692ef603ee14152d8
1sudo <zjmw00@gmail.com>
2020-06-04 23:32:56 -0500

    [Added] MTG mobiles and objects

--- MMOCoreORB/bin/scripts/object/tangible/base/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/base/serverobjects.lua
@@ -48,3 +48,5 @@
 includeFile("tangible/base/tangible_base.lua")
 includeFile("tangible/base/tangible_craftable.lua")
 includeFile("tangible/base/tangible_default.lua")
+includeFile("tangible/base/retrieve_quest_base.lua")
+includeFile("tangible/base/holobeast_base.lua")
