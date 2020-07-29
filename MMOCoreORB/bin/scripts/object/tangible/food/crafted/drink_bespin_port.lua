a7063127f75d12036d405afa05b1ca55bfb7d786
dasboogles <r.jbenson01@gmail.com>
2020-07-28 20:15:18 -0400

    Removed filling from Bespin Port and Pyo Cakes.

--- MMOCoreORB/bin/scripts/object/tangible/food/crafted/drink_bespin_port.lua
+++ MMOCoreORB/bin/scripts/object/tangible/food/crafted/drink_bespin_port.lua
@@ -45,14 +45,14 @@ object_tangible_food_crafted_drink_bespin_port = object_tangible_food_crafted_sh
 	templateType = CONSUMABLE,
 
 	duration = 1,
-	filling = 10,
-	nutrition = 10,
+	filling = 1,
+	nutrition = 12,
 
 	effectType = 3, -- Event Based Buff
 	eventTypes = {CRAFTINGEXPERIMENTATION},
 
-	fillingMin = 25,
-	fillingMax = 15,
+	fillingMin = 1,
+	fillingMax = 1,
 	flavorMin = 1,
 	flavorMax = 1,
 	nutritionMin = 3,
