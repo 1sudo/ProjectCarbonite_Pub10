b5caef55e196b430a7775517b80144bb456ee206
dasboogles <r.jbenson01@gmail.com>
2020-11-13 22:51:41 -0500

    Increasing requirements on resources for SEA removal tools.

--- MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
@@ -19,12 +19,12 @@ object_draft_schematic_item_item_sea_removal_tool = object_draft_schematic_item_
     customizationStringNames = {},
     customizationDefaults = {},
  
-    ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
+    ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
     ingredientTitleNames = {"assembly_enclosure", "thermal_shielding", "geonosian_power_cube", "clasp", "clothing_attachment", "armor_attachment"},
-    ingredientSlotType = {0, 0, 1, 1, 1, 1},
-    resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff", "object/tangible/slicing/shared_slicing_molecular_clamp.iff", "object/tangible/gem/shared_clothing.iff", "object/tangible/gem/shared_armor.iff"},
-    resourceQuantities = {10, 5, 1, 2, 1, 1},
-    contribution = {100, 100, 100, 100, 100, 100},
+    ingredientSlotType = {0, 0, 1},
+    resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff"},
+    resourceQuantities = {500, 250, 1},
+    contribution = {100, 100, 100},
  
     targetTemplate = "object/tangible/item/clothing_sea_removal_tool.iff",
  
